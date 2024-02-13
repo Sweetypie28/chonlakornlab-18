fe32a4e0a94d8c5f6eb5e75026a22026310655b5
Sweetypie <khaw2802@gmail.com>
2024-02-13 16:13:38 +0700

    yeahh

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+	double Lx = min(R1 -> x + R1 -> w, R2 -> x + R2 -> w) - max(R1 -> x, R2 -> x);
+	double Ly = min(R1 -> y, R2 -> y) - max(R1 -> y - R1 -> h, R2 -> y - R2 -> h);
+	if(Lx <=0) return 0;
+	return Lx*Ly ;
+}
 
-
-
+int main(){
+	Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};
+	cout << overlap(&R1,&R2);
 }
